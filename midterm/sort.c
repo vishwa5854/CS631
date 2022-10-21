2681b00760cf143ba4ac32383df7510555b2e6bf
vish <vishwanath5854@outlook.com>
2022-10-21 16:30:50 -0400

    Added separate .c and .h files for sort

---
+++ midterm/sort.c
@@ -0,0 +1,60 @@
+#include<sys/stat.h>
+
+#include"sort.h"
+#include<string.h>
+
+int sort(const FTSENT** one, const FTSENT** two, struct SORT_FLAGS *sort_flags) {
+    /** f is the KING, it will override all the sort options irrespective of the order. */
+    if (sort_flags->f) {
+        return 0;
+    }
+
+    if (sort_flags->t || sort_flags->l) {
+        if (sort_flags->c) {
+            /** Lexicographical sort in case of equal values of ctime */
+            if ((*one)->fts_statp->st_ctime == (*two)->fts_statp->st_ctime) {
+                return sort_flags->r ? 
+                            strcmp((*two)->fts_name, (*one)->fts_name) :
+                            strcmp((*one)->fts_name, (*two)->fts_name);
+            }
+            return sort_flags->r ? 
+                        ((*two)->fts_statp->st_ctime < (*one)->fts_statp->st_ctime) : 
+                        ((*one)->fts_statp->st_ctime < (*two)->fts_statp->st_ctime);
+        }
+
+        if (sort_flags->u) {
+            if ((*one)->fts_statp->st_atime == (*two)->fts_statp->st_atime) {
+                return sort_flags->r ? 
+                            strcmp((*two)->fts_name, (*one)->fts_name) :
+                            strcmp((*one)->fts_name, (*two)->fts_name);
+            }
+            return sort_flags->r ? 
+                        ((*two)->fts_statp->st_atime < (*one)->fts_statp->st_atime) :
+                        ((*one)->fts_statp->st_atime < (*two)->fts_statp->st_atime);
+        }
+
+        if (sort_flags->t) {
+            if ((*one)->fts_statp->st_mtime == (*two)->fts_statp->st_mtime) {
+                return sort_flags->r ? 
+                            strcmp((*two)->fts_name, (*one)->fts_name) :
+                            strcmp((*one)->fts_name, (*two)->fts_name);
+            }
+            return sort_flags->r ? 
+                        ((*two)->fts_statp->st_mtime < (*one)->fts_statp->st_mtime) : 
+                        ((*one)->fts_statp->st_mtime < (*two)->fts_statp->st_mtime);
+        }
+    }
+
+    if (sort_flags->S) {
+        if ((*one)->fts_statp->st_size == (*two)->fts_statp->st_size) {
+            return sort_flags->r ? 
+                        strcmp((*two)->fts_name, (*one)->fts_name) :
+                        strcmp((*one)->fts_name, (*two)->fts_name);
+        }
+        return sort_flags->r ? 
+                    ((*two)->fts_statp->st_size < (*one)->fts_statp->st_size) : 
+                    ((*one)->fts_statp->st_size < (*two)->fts_statp->st_size);
+    }
+    return sort_flags->r ? strcmp((*two)->fts_name, (*one)->fts_name)
+                   : strcmp((*one)->fts_name, (*two)->fts_name);
+}
---
+++ midterm/sort.h
@@ -0,0 +1,14 @@
+#include<fts.h>
+#include<stdbool.h>
+
+struct SORT_FLAGS {
+    bool f;
+    bool S;
+    bool t;
+    bool c;
+    bool u;
+    bool r;
+    bool l;
+};
+
+int sort(const FTSENT** one, const FTSENT** two, struct SORT_FLAGS *sort_flags);
